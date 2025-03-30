eb379a3d5b7a3ce451c2c68e23b5785b21f4d3e7
pedroGEOGIScoding <pedromartinezduran@gmail.com>
2025-03-30 11:38:50 +0200

    Create additional jsx for update, create and detail items

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Home from './pages/Home.jsx';
 import Navbar from './components/Navbar.jsx';
-import Flights from './pages/Flights.jsx';
+import Flights from './Flights/Flights.jsx';
 import MapFlights from './pages/MapFlights.jsx';
-import Planes from './pages/Planes.jsx';
-import Airports from './pages/Airports.jsx';
+import Planes from './Planes/Planes.jsx';
+import Airports from './Airports/Airports.jsx';
 import About from './pages/About.jsx';
 import { Container } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
